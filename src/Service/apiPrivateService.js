22b852ae95260c0beb2577a234d61da0505cef50
Lucas Gomes <lmgomes96@gmail.com>
2021-02-04 03:47:40 -0300

    fix errors on url

--- src/Service/apiPrivateService.js
+++ src/Service/apiPrivateService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'https://django-backend-canil.azurewebsites.net/';
+const API_URL = 'https://django-backend-canil.azurewebsites.net/api/private/';
 /* const API_URL = 'http://localhost:8000/private/'; 
  */
 
