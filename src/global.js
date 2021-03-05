95e0a6d4cbf6c1576bceb1ea2811af845b6d37da
gomeslucasm <lmgomes96@gmail.com>
2021-03-05 13:08:55 -0300

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
 /* var API_URL = 'https://django-backend-canil.azurewebsites.net'; */
-const API_URL = 'http://192.168.1.108:8000'; 
+const API_URL = 'https://192.168.1.108:8000'; 
 export default API_URL;
